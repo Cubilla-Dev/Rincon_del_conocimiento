38b0ddb5bf59cb17db8159a78c2d6e73d5fe3f09
Cubilla-Dev <137528287+Cubilla-Dev@users.noreply.github.com>
2025-06-05 07:51:55 -0300

    Create entities Guide.js

---
+++ src/core/entities/Guide.js
@@ -0,0 +1,7 @@
+Class Guide {
+constructor(title, steps, equipment, isVerified = false) {
+   this.title = title;
+   this.steps = steps;
+  this.equipment = equipment;
+  this.isVerified = isVerified;
+}}
